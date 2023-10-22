c91675715fd96b493cb2f79ae2cd5ee827dd5904
Artem Trehub <tregubartemofficial@gmail.com>
2023-10-23 02:41:16 +0300

    working google calendar API

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,44 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setIsLoggedIn, setUser } from "../redux/userSlice";
 
 const Header = () => {
-  const google = useGoogleLogin({
+  const [accessToken, setAccessToken] = useState(null);
+  const dispatch = useDispatch();
+
+  const googleLogin = useGoogleLogin({
+    clientId:
+      "117651172871-gbr6jcrh7l50rrtp3kedr9dsbjr9lob4.apps.googleusercontent.com",
     onSuccess: async (tokenResponse) => {
       const userInfo = await axios
         .get("https://www.googleapis.com/oauth2/v3/userinfo", {
           headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
         })
         .then((res) => res.data);
-      fetchUserCalendar(userInfo, tokenResponse);
+      localStorage.setItem("access_token", tokenResponse.access_token);
+      dispatch(setUser({ userInfo }));
+      dispatch(setIsLoggedIn(true));
+      setAccessToken(tokenResponse.access_token);
     },
+    scope:
+      "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar.acls",
   });
 
-  const fetchUserCalendar = async (userInfo, tokenResponse) => {
-    console.log("User's information:", userInfo);
-    console.log("Token response:", tokenResponse);
-    if (userInfo && userInfo.sub) {
-      const calendarInfo = axios.get(
-        `https://www.googleapis.com/calendar/v3/calendars/${userInfo.sub}`,
-        {
-          headers: {
-            Authorization: `Bearer ${tokenResponse.access_token}`,
-          },
-        }
-      );
-      console.log("User's Calendar Info:", calendarInfo.data);
-    } else {
-      console.log("User's information is not available.");
+  const googleLogout = async () => {
+    try {
+      await axios.post("https://accounts.google.com/o/oauth2/revoke", null, {
+        params: {
+          token: accessToken,
+        },
+      });
+      localStorage.removeItem("access_token");
+      dispatch(setIsLoggedIn(false));
+      dispatch(setUser({}));
+      setAccessToken(null);
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -50,8 +60,16 @@ const Header = () => {
         >
           Movies
         </Link>
+        {accessToken ? (
+          <button className="button" onClick={googleLogout}>
+            Sign out
+          </button>
+        ) : (
+          <button className="button" onClick={googleLogin}>
+            Sign in
+          </button>
+        )}
       </nav>
-      <button onClick={google}>Sign in</button>
     </header>
   );
 };
